b7dc3b5e330f0616ec99c688fa7f00decd04819f
Furkan Demir <furkan.vty0@gmail.com>
2023-04-02 14:06:07 +0300

    Initial commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject spherePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+            Instantiate(cubePrefab, this.gameObject.transform.position, Quaternion.identity);
+        else if(Random.Range(0,100)<10)
+            Instantiate(spherePrefab, this.gameObject.transform.position, Quaternion.identity);
+    }
+}
